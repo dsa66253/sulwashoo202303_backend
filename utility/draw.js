49298e2e55951b9ceddc8c5c7fe854b71dfb0aab
Mary <dsa66253@yahoo.com.tw>
2023-04-11 12:02:42 +0800

    remove unused code

--- utility/draw.js
+++ utility/draw.js
@@ -23,7 +23,7 @@ const getAwardBucket = ()=>{
     for(let k in awardRatio){
         awardNorRatio[k] = parseInt(awardRatio[k]*multiplier)
     }
-    console.log(awardNorRatio)
+    // console.log(awardNorRatio)
     //info create bucket
     let awardBucket = {}
     let minBucketNum = 1
@@ -42,7 +42,6 @@ const getAwardBucket = ()=>{
     return {awardBucket, minBucketNum, maxBucketNum}
 }
 let {awardBucket, minBucketNum, maxBucketNum} = getAwardBucket()
-console.log(awardBucket, minBucketNum, maxBucketNum)
 const playDraw = async (transactionConn)=>{
     // return string type of gift number
     // play draw by transaction connection
